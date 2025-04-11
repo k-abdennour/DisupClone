fd64460f1881c8aa1f2be3897475af264bb5e741
Abdennour <k.abdennour@alexsys.solutions>
2025-04-11 12:33:49 +0100

    fix Auth

--- src/app/modules/auth/AuthLayout.tsx
+++ src/app/modules/auth/AuthLayout.tsx
@@ -1,104 +1,72 @@
-
-import {useEffect} from 'react'
-import {Outlet, Link} from 'react-router-dom'
-import {toAbsoluteUrl} from '../../../_metronic/helpers'
+import { useEffect } from "react";
+import { Outlet, Link } from "react-router-dom";
+import { toAbsoluteUrl } from "../../../_metronic/helpers";
 
 const AuthLayout = () => {
   useEffect(() => {
-    const root = document.getElementById('root')
+    const root = document.getElementById("root");
     if (root) {
-      root.style.height = '100%'
+      root.style.height = "100%";
     }
     return () => {
       if (root) {
-        root.style.height = 'auto'
+        root.style.height = "auto";
       }
-    }
-  }, [])
+    };
+  }, []);
 
   return (
-    <div className='d-flex flex-column flex-lg-row flex-column-fluid h-100'>
+    <div className="d-flex flex-column flex-lg-row flex-column-fluid h-100">
       {/* begin::Body */}
-      <div className='d-flex flex-column flex-lg-row-fluid w-lg-50 p-10 order-2 order-lg-1'>
+      <div className="d-flex flex-column flex-lg-row-fluid w-lg-50 p-10 order-2 order-lg-1">
         {/* begin::Form */}
-        <div className='d-flex flex-center flex-column flex-lg-row-fluid'>
+        <div className="d-flex flex-center flex-column flex-lg-row-fluid">
           {/* begin::Wrapper */}
-          <div className='w-lg-500px p-10'>
+          <div className="w-lg-500px p-10">
             <Outlet />
           </div>
-          {/* end::Wrapper */}
         </div>
-        {/* end::Form */}
-
-        {/* begin::Footer */}
-        <div className='d-flex flex-center flex-wrap px-5'>
-          {/* begin::Links */}
-          <div className='d-flex fw-semibold text-primary fs-base'>
-            <a href='#' className='px-5' target='_blank'>
-              Terms
-            </a>
-
-            <a href='#' className='px-5' target='_blank'>
-              Plans
-            </a>
-
-            <a href='#' className='px-5' target='_blank'>
-              Contact Us
-            </a>
-          </div>
-          {/* end::Links */}
-        </div>
-        {/* end::Footer */}
       </div>
       {/* end::Body */}
 
       {/* begin::Aside */}
       <div
-        className='d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2'
-        style={{backgroundImage: `url(${'/media/misc/auth-bg.png'})`}}
+        className="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2 position-relative"
+        style={{
+          backgroundImage: `url('https://raw.githubusercontent.com/student726/assets/main/sonasid-cover.png')`,
+        }}
       >
-        {/* begin::Content */}
-        <div className='d-flex flex-column flex-center py-15 px-5 px-md-15 w-100'>
-          {/* begin::Logo */}
-          <Link to='/' className='mb-12'>
-            <img alt='Logo' src={'/media/logos/custom-1.png'} className='h-75px' />
-          </Link>
-          {/* end::Logo */}
-
-          {/* begin::Image */}
-          <img
-            className='mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20'
-            src={'/media/misc/auth-screens.png'}
-            alt=''
-          />
-          {/* end::Image */}
-
-          {/* begin::Title */}
-          <h1 className='text-white fs-2qx fw-bolder text-center mb-7'>
-            Fast, Efficient and Productive
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0, 0, 0, 0.4)", // overlay sombre
+            zIndex: 1,
+          }}
+        />
+        <div
+          className="d-flex flex-column flex-center py-15 px-5 px-md-15 w-100"
+          style={{ zIndex: 2, position: "relative" }}
+        >
+          <h1
+            className="text-white fw-bold text-center mb-7"
+            style={{ fontSize: "2.75rem", lineHeight: "1.4" }}
+          >
+            BIENVENUE
+            <br />
+            SUR VOTRE PLATEFORME
+            <br />
+            DIS-UP
           </h1>
-          {/* end::Title */}
-
-          {/* begin::Text */}
-          <div className='text-white fs-base text-center'>
-            In this kind of post,{' '}
-            <a href='#' className='opacity-75-hover text-warning fw-bold me-1'>
-              the blogger
-            </a>
-            introduces a person they’ve interviewed <br /> and provides some background information
-            about
-            <a href='#' className='opacity-75-hover text-warning fw-bold me-1'>
-              the interviewee
-            </a>
-            and their <br /> work following this is a transcript of the interview.
-          </div>
-          {/* end::Text */}
         </div>
-        {/* end::Content */}
       </div>
+
       {/* end::Aside */}
     </div>
-  )
-}
+  );
+};
 
-export {AuthLayout}
+export { AuthLayout };
